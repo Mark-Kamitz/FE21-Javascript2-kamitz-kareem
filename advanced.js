1990792362a688523f799a97162b60f05370e89a
markus kamitz <markus_kamitz@yahoo.de>
2021-06-24 14:05:58 +0200

    intermediate1 updated

--- advanced.js
+++ advanced.js
@@ -1,11 +1,11 @@
 //advanced
-function calculate(minutes){
- var hours = Math.floor( minutes / 60 );
- var minutes = minutes - ( hours * 60 );
- return [hours,minutes];
-}
+// function calculate(minutes){
+//  var hours = Math.floor( minutes / 60 );
+//  var minutes = minutes - ( hours * 60 );
+//  return [hours,minutes];
+// }
 
-calculate(200);
-document.write("Hours: " + calculate(200)[0] + "<br>");
-document.write("Minutes: " + calculate(200)[1]);
+// calculate(200);
+// document.write("Hours: " + calculate(200)[0] + "<br>");
+// document.write("Minutes: " + calculate(200)[1]);
 
--- intermediate.js
+++ intermediate.js
@@ -1,11 +1,10 @@
 // intermediate1
-//     (function(argument) {  
-//     var first_letter = argument.charAt(0);
-//     first_letter = first_letter.toUpperCase();
-//     argument = argument.substring(1);
-//     var output = first_letter + argument;
-//     return  document.write(output);
-//    })('i am a web developer' );
+    (function(argument) {  
+    var first_letter = argument.charAt(0).toUpperCase();;
+    argument = argument.substring(1);
+    var output = first_letter + argument;
+    document.write(output);
+   })('i am a web developer' );
 
 
 // intermediate2
