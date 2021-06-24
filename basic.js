8b6048f0eb5dd7e248561378c032e3a414839696
markus kamitz <markus_kamitz@yahoo.de>
2021-06-24 11:54:48 +0200

    basic + intermediate1

---
+++ basic.js
@@ -0,0 +1,45 @@
+
+// basic1
+// function crystalGazer(job_title,location,partners_name,number_childrens){
+//  return (`You will be a ${job_title} in ${location} and married to ${partners_name} with ${number_childrens} children.`);
+// }
+// var something = crystalGazer("sleepy","vienna","beauty",0);
+// document.write(something);
+
+//basic2
+
+// function ageCalculator(birth_year,current_year){
+//     var year = current_year - birth_year;
+//     return ( `You are either ${year} or ${year-1} `);
+// }
+// document.write(ageCalculator(1991,2021));
+
+//basic 3
+
+// function ageCalculator(birth_year){
+//     const d = new Date();
+//     var year = d.getFullYear() - birth_year;
+//     return ( `You are either ${year} or ${year-1} `);
+// }
+// document.write(ageCalculator(1991));
+
+//basic 4
+
+// function degree_to_radiant(degree){
+//     var radiant =  degree*(Math.PI/180);
+//     return(radiant +` radians`);
+// }
+// document.write(degree_to_radiant(20));
+
+//basic 5
+
+// function area_of_box(width,height,depth){
+//     var area = width * height;
+//     var volume = width * height * depth;
+//     return [area,volume];
+// }
+
+// document.write("<span style='color:red'>The area  of the box is: </span>" + area_of_box(20,20,20)[0] + "<br>");
+// document.write("The volume  of the box is: " + area_of_box(20,20,20)[1]);
+
+
